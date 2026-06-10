1c909c6db8ae800fa45c3ab63ae35011ad57e3b0
SonieshImmidi <soniesh1102@gmail.com>
2026-06-10 21:27:51 +0530

    change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import store from '../store';
 import { logout, setCredentials } from '../store/authSlice';
 
-const API_BASE = 'http://localhost:8000';
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
 const api = axios.create({
   baseURL: API_BASE,
